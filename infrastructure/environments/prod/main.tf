17c429bd828c3d97585be12639f05f2d233d948b
Matialv15 <Matialv15@gmail.com>
2026-06-20 17:01:40 -0300

    retry2

--- infrastructure/environments/prod/main.tf
+++ infrastructure/environments/prod/main.tf
@@ -2,7 +2,9 @@ terraform {
   required_providers {
     aws = { source = "hashicorp/aws", version = "~> 5.0" }
   }
-  backend "s3" {}
+  backend "local" {
+    path = "terraform.tfstate"
+  }
 }
 
 provider "aws" {
