b6ed0ba9e6cb7cdab6f364da9ae4c7802a33cbcb
Matialv15 <Matialv15@gmail.com>
2026-06-20 19:12:25 -0300

    fix route table

--- infrastructure/modules/networking/main.tf
+++ infrastructure/modules/networking/main.tf
@@ -90,13 +90,13 @@ resource "aws_route_table" "private" {
 }
 
 resource "aws_route_table_association" "public" {
-  count          = length(aws_subnet.public)
+  count          = length(local.azs)
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "private" {
-  count          = length(aws_subnet.private)
+  count          = length(local.azs)
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
 }
